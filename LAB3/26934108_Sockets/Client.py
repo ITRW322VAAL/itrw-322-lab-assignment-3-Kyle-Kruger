5c1e2c88481202fa0925413094da46d445a6fcac
KJ Kruger 26934108 <kyle.john.kruger@gmail.com>
2019-10-07 11:59:00 +0200

    26934108 Initialized Client.py

---
+++ LAB3/26934108_Sockets/Client.py
@@ -0,0 +1,14 @@
+#!/user/bin/env python3
+
+import socket
+
+HOST ='127.0.0.1' # The servers hostname or IP address
+PORT = 9999 # The port used by the server
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST,PORT))
+    s.sendall(b'Hello, Server')
+    data = s.recv(1024)
+
+print('Recieved', repr(data))
+
