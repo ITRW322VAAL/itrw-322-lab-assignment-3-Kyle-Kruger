85c7245cdeeac65d1c069b981a092c7008129aff
KJ Kruger 26934108 <kyle.john.kruger@gmail.com>
2019-10-07 11:51:34 +0200

    26934108 Added and Edited Server.py. Created 26934208_Sockets

---
+++ LAB3/26934108_Sockets/Server.py
@@ -0,0 +1,19 @@
+#!/user/bin/env python3
+
+import socket
+
+HOST = '127.0.0.1' # Standard Loop Back interface address (localhost)
+PORT = 9999 # Port to Listen on
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen
+    conn, addr = s.accept()
+    with conn:
+        print('Server is connected with address', addr)
+        while True:
+            data = conn.recv(2048) #default byte stream size
+            if not data:
+                break
+            conn.sendall(data)
+
